19ddec3033afa6f047214f7751bbef9bbf736247
Cheer Xiao <xiaqqaix@gmail.com>
2013-10-05 21:56:21 +0800

    Fix dependency Flask==0.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup_args = dict(
     name='tunaweb',
     install_requires=[
-        'Flask>=0.8',
+        'Flask==0.9',
         'flatland==dev',
     ],
     entry_points=dict(
